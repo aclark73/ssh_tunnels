fb9ef14147aa0d1f557b81df481997e961bfcf68
Adam Clark <adam.h.clark@gmail.com>
2019-06-04 22:43:19 -0700

    Alternate syntax

--- tunnels.sh
+++ tunnels.sh
@@ -32,13 +32,43 @@ function ssht_tunnel () {
 	port=$2
 	remote_port=$3
 
-	echo Tunneling to $host : $remote_port on $port 
-	
+	echo Tunneling to $host : $remote_port on $port
+
 	ps_found=`ps ax | grep ssh | grep "$port:$host"`
 	if [ -z "$ps_found" ]
 	then
 		ssh -p $ssht_gateway_port -f $ssht_gateway_userhost -L $port:$host:$remote_port -N
 	fi
+}
+
+# ssht_define2 user@host:port local_port
+# eg. ssht_define2 www@host1 2201
+# eg. ssht_define2 host1:80 8001
+function ssht_define2 () {
+	name=$1
+	remote=$2
+	local_port=$3
+
+	arguser=`cut -d '@' -f1 <<< $remote`
+	hostport=`cut -d '@' -f2 <<< $remote`
+	arghost=`cut -d ':' -f1 <<< $hostport`
+	argport=`cut -d ':' -f2 <<< $hostport`
+
+	user=${arguser-$ssht_ssh_user}
+	host=${arghost}
+	port=${argport-22}
+
+	full_host=`ssht_host $host`
+
+	tunnel_cmd="ssht_tunnel $full_host $local_port $port"
+	ssh_cmd="$tunnel_cmd; ssh -p $local_port $user@localhost"
+
+	# Shortcuts
+	eval ${ssht_prefix}_${name}_userhost='$user@localhost'
+	eval ${ssht_prefix}_${name}_port='$local_port'
+	alias ${ssht_prefix}_${name}_tunnel="$tunnel_cmd"
+	alias ${ssht_prefix}_${name}_ssh="$ssh_cmd"
+
 }
 
 function ssht_define () {
@@ -74,8 +104,6 @@ function ssht_define () {
 
 # Map port 80 on each of several web servers to local port 8x80
 # http://localhost:8180/some/url hits the server directly
-# ssht_define ws1 ws1.internal.domain.org 8180 80
+# ssht_define ws1 ws1.internal.domain.org:80 :8180
 # ssht_define ws2 ws2.internal.domain.org 8280 80
 # ssht_define ws3 ws3.internal.domain.org 8380 80
-
-
